f5c46d7e9fea72ff623f99eeb823e08044016144
edwin <edwinosayande@gmail.com>
2023-07-19 22:52:00 +0100

    budget office project

---
+++ src/data.js
@@ -0,0 +1,70 @@
+
+export const offerData = [
+    {
+        id:1,
+        quantity:1,
+        title:"730 Days Savings Bond",
+        minimum: 5000,
+        percentagePerYear:"11.382",
+        interestPayment: "Quarterly",
+        opening:"Oct/07/2023",
+        closing:"Oct/12/2023",
+        settlement:"Oct/27/2023"
+    },
+    {
+        id:2,
+        quantity:1,
+        title:"360 Days Savings Bond",
+        minimum: 4000,
+        percentagePerYear:"11.382",
+        interestPayment: "Quarterly",
+        opening:"Oct/23/2023",
+        closing:"Aug/10/2023",
+        settlement:"Oct/27/2023"
+    },
+    {
+        id:3,
+        quantity:1, 
+        title:"430 Days Savings Bond",
+        minimum: 3000,
+        percentagePerYear:"11.382",
+        interestPayment: "Quarterly",
+        opening:"Dec/21/2023",
+        closing:"Nov/18/2023",
+        settlement:"Oct/07/2023"
+    },
+    {
+        id:4,
+        quantity:1, 
+        title:"730 Days Savings Bond",
+        minimum: 6000,
+        percentagePerYear:"10.382",
+        interestPayment: "Quarterly",
+        opening:"Oct/07/2023",
+        closing:"Oct/12/2023",
+        settlement:"Oct/27/2023"
+    },
+    {
+        id:5,
+        quantity:1, 
+        title:"730 Days Savings Bond",
+        minimum: 7000,
+        percentagePerYear:"11.382",
+        interestPayment: "Quarterly",
+        opening:"Oct/07/2023",
+        closing:"Oct/12/2023",
+        settlement:"Oct/27/2023"
+    },
+    {
+        id:6,
+        quantity:1, 
+        title:"330 Days Savings Bond",
+        minimum: 8000,
+        percentagePerYear:"11.382",
+        interestPayment: "Quarterly",
+        opening:"Oct/07/2023",
+        closing:"Oct/12/2023",
+        settlement:"Oct/27/2023"
+    },
+    
+];
